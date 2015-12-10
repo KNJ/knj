1800b64bd5c24afd6a1d80125cdb369cc645fd34
KNJ <knj@wazly.net>
2015-12-10 12:23:26 +0900

    Add statement to source NVM

--- .bash/startup.sh
+++ .bash/startup.sh
@@ -1,4 +1,9 @@
+# rbenv
 if [ -d ~/.rbenv ]; then
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
 fi
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
