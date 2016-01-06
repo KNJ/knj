b4f31884c13b187ef11c0d201b73d31e25a74877
KNJ <knj@wazly.net>
2016-01-06 15:25:22 +0000

    Add git configuration to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 sudo yum update -y
 sudo yum install -y gcc openssl-devel zlib-devel readline-devel
 
+# Git
+git config --global user.name "KNJ"
+git config --global user.email "knj@wazly.net"
+git config --global color.ui true
+git config --global grep.lineNumber true
+git config --global alias.find 'grep -H --heading --break'
+
 # rbenv
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
