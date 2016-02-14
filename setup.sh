460a6396fbcf5ee54d7eb5577844032ed53381fc
KNJ <knj@wazly.net>
2016-02-14 18:47:52 +0900

    Add PHP and Composer

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 sudo yum update -y
 
 sudo cp ~/templates/nginx.repo /etc/yum.repos.d/
+sudo cp ~/templates/Mariadb.repo /etc/yum/repos.d/
 
-sudo yum install -y vim nginx mariadb mariadb-devel
+sudo yum install -y vim nginx MariaDB-server MariaDB-client
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
@@ -28,6 +29,14 @@ git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/r
 
 # NVM
 git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
+
+# PHP
+sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sudo yum install -y --enable-repo=remi-php70 php php-devel php-mysql php-gd php-mbstring php-intl php-pecl-yaml
+sudo systemctl start php-fpmsudo systemctl enable php-fpm
+
+# Composer
+curl -sS https://getcomposer.org/installer | php
 
 source ~/.bash_profile
 cd
